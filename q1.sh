79e50cdbe372f3e6cc96f6e1967bb073cdfbd274
bhanujggandhi <gandhibhanuj@gmail.com>
2022-08-11 15:56:52 +0530

    minor tweaks

--- q1.sh
+++ q1.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Name: Bhanuj Gandhi
+# Roll no: 2022201068
+
 n=$(wc $1 | awk '{print $1}')
 
 awk -v a="$n" 'NR==int(a/2 + 0.5) {print $0}' $1
--- q2.sh
+++ q2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-awk '/\/usr/ {print $0}' /etc/shells | cut -d '/' -f 4
+# Name: Bhanuj Gandhi
+# Roll No.: 2022021068
 
-awk -F'/' '/\/usr/ {print $4}' /etc/shells
+#awk -F/ '/\/usr/ {print $4}' /etc/shells
